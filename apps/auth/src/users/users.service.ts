25d640afd3d9486cf70f199a23dd3013e9281d56
fabogit <fabo.dev.email@gmail.com>
2025-02-18 11:09:59 +0100

    #6
    user controller and service

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,26 +1,97 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Injectable,
+  OnModuleInit,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  CreateUserDto,
+  UpdateUserDto,
+  User,
+  Users,
+  PaginationDto,
+} from '@app/common';
+import { Observable, Subject } from 'rxjs';
+import { randomUUID } from 'crypto';
+import { faker } from '@faker-js/faker';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UsersService implements OnModuleInit {
+  private readonly users: User[] = [];
+  private readonly logger = new Logger(UsersService.name, { timestamp: true });
+
+  onModuleInit() {
+    const number = 500;
+    this.logger.log(`Creating ${number} Users...`);
+    for (let i = 0; i < number; i++) {
+      this.create({
+        username: faker.internet.username(),
+        password: faker.internet.password(), //for development it's okay
+        age: faker.number.int({ min: 18, max: 99 }),
+      });
+    }
+    this.logger.log('Done');
   }
 
-  findAll() {
-    return `This action returns all users`;
+  create(createUserDto: CreateUserDto): User {
+    const user: User = {
+      ...createUserDto,
+      isSubscribed: false,
+      socialMedia: {},
+      id: randomUUID(),
+    };
+    this.users.push(user);
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findAll(): Users {
+    return { users: this.users };
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  findOne(id: string): User {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User not found by id: ${id}`);
+    }
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      this.users[userIndex] = {
+        ...this.users[userIndex],
+        ...updateUserDto,
+      };
+      return this.users[userIndex];
+    }
+    throw new NotFoundException(`User not found by id: ${id}`);
+  }
+
+  remove(id: string) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      return this.users.splice(userIndex, 1)[0];
+    }
+    throw new NotFoundException(`User not found by id: ${id}`);
+  }
+
+  queryUsers(
+    paginationDtoStream: Observable<PaginationDto>,
+  ): Observable<Users> {
+    const subject = new Subject<Users>();
+
+    const onNext = (paginationDto: PaginationDto) => {
+      const start = paginationDto.page * paginationDto.skip;
+
+      subject.next({
+        users: this.users.slice(start, start + paginationDto.skip),
+      });
+    };
+    const onComplete = () => subject.complete();
+
+    paginationDtoStream.subscribe({ next: onNext, complete: onComplete });
+
+    return subject.asObservable();
   }
 }
