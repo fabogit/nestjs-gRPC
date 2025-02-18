be40ae6cc031f3cb6f3aa6406d46e12ed8cb7359
fabogit <fabo.dev.email@gmail.com>
2025-02-18 09:50:12 +0100

    #5
    start microservice on grpc transport

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { AUTH } from '@app/common';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        // path to proto when compiled
+        protoPath: join(__dirname, '../auth.proto'),
+        package: AUTH,
+      },
+    },
+  );
+  await app.listen();
 }
-bootstrap();
+
+bootstrap().catch((err) => console.error(err));
